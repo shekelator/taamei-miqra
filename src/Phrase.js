87b7227317ac902b78ff4c9b1a07b91867443c72
David Nichol <shekelator@gmail.com>
2022-11-06 12:39:55 -0500

    Trying CDN for audio

--- src/Phrase.js
+++ src/Phrase.js
@@ -8,7 +8,7 @@ function Phrase(props) {
             </div>
             <div className="col text-start">
                 <audio controls>
-                    <source src={ audioFile } type="audio/mp3"/>
+                    <source src=https://dnix.nyc3.cdn.digitaloceanspaces.com/recordings/{ audioFile } type="audio/mp3"/>
                 </audio>
             </div>
         </div>
